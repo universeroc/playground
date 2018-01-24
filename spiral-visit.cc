8115b827afd741038c145a0f13d19e8b9923b272
universeroc <universeroc@gmail.com>
2018-01-24 11:29:32 +0800

    http://cpptruths.blogspot.com/2011/10/multi-dimensional-arrays-in-c11.html

--- spiral-visit.cc
+++ spiral-visit.cc
@@ -1,10 +1,16 @@
-
 #include <array>
 #include <cassert>
 
+// http://cpptruths.blogspot.com/2011/10/multi-dimensional-arrays-in-c11.html
 template<class T, size_t R, size_t C>
-std::array<T, R*C> SpiralVisit(const std::array<std::array<T, C>, R>& a) {
-  std::array<T, R*C> o;
+using Mat2 = std::array<std::array<T, C>, R>;
+
+template<class T, size_t L>
+using Mat1 = std::array<T, L>;
+
+template<class T, size_t R, size_t C>
+Mat1<T, R*C> SpiralVisit(const Mat2<T,R,C>& a) {
+  Mat1<T, R*C> o;
   size_t oc = 0;
   for (int i = 0, j = 0; i < R / 2 + 1 && j < C / 2 + 1 && oc < R*C; i++, j++) {
     auto r = i, c = j;
@@ -37,21 +43,21 @@ std::array<T, R*C> SpiralVisit(const std::array<std::array<T, C>, R>& a) {
 
 int main()
 {
-  std::array<std::array<int, 3>, 2> a1 = { {
+  Mat2<int, 2, 3> a1 = { {
     { { 1,2,3 } },
     { { 4,5,6 } }
-    } };
+  } };
 
   auto a1o = SpiralVisit<int, 2, 3>(a1);
-  std::array<int, 6> a1oe = { 1,2,3,6,5,4 };
+  Mat1<int, 6> a1oe = { 1,2,3,6,5,4 };
   assert(a1o == a1oe);
 
-  std::array<std::array<int, 3>, 3> a2 = { {
+  Mat2<int, 3, 3> a2 = { {
     { { 1, 2, 3 } },
     { { 4, 5, 6 } },
     { { 7, 8, 9 } }
-    } };
-  std::array<int, 9> a2oe = { 1,2,3,6,9,8,7,4,5 };
+  } };
+  Mat1<int, 9> a2oe = { 1,2,3,6,9,8,7,4,5 };
   auto a2o = SpiralVisit<int, 3, 3>(a2);
   assert(a2o == a2oe);
 
